070b0a9699cef49ade97d14105980de9f5d0e856
Richard Klancer <rpk@pobox.com>
2010-06-21 16:01:56 -0400

    Updated raphael event delegation test to reflect new understanding. Previous examples did not work in IE

--- test-raphael-group.js
+++ test-raphael-group.js
@@ -1,25 +1,26 @@
 /*globals $ Raphael */
 
 // make these accessible at the Firebug/Web Inspector console
-var r, group;
+var r, group, points;
 
 $(function () {
   
   var notHighlighted = {
-        stroke: "#aaaaaa",
-        fill: "#aaaaaa"
+        stroke: "#999999",
+        fill: "#999999"
       },
+      
       highlighted = {
         stroke: "#aa0000",
         fill: "#aa0000"            
       },
+      
       notClicked = {
-        opacity: 0.5,
-        r: 6
+        opacity: 0.3
       },
+      
       clicked = {
-        opacity: 1.0, 
-        r: 8
+        opacity: 1.0
       };
 
   var $container = $("#container"),
@@ -29,21 +30,45 @@ $(function () {
   // set up the Raphael 'canvas' and add 3 horizontally-alighted points to it.
   
   r = Raphael($container[0]);       
-
-  var points = [r.circle(w/2 + 50, h/2), r.circle(w/2, h/2), r.circle(w/2 - 50, h/2)];
-  $.each(points, function (index, point) { point.attr(notClicked).attr(notHighlighted); });
+  
+  points = [r.circle(w/2 + 50, h/2, 6), 
+    r.path("M " + (w/2-6) + " " + (h/2-6) + "l 0 12 l 12 0 l 0 -12 z"), 
+    r.circle(w/2 - 50, h/2, 6)];
   
   
   // interpose a group node (using SVG or VML semantics as appropriate) between the canvas & the 3 points
   
   group = r.raphael.vml ? 
-    document.createElement("vml:group") : 
+    document.createElement("group") : 
     document.createElementNS("http://www.w3.org/2000/svg", "g"); // (jQuery can't create namespaced DOM nodes)
-
-  var $group = $(group);
-  $(r.canvas).append($group);
-  $.each(points, function (index, point) { $group.append($(point.node)); });
-
+  
+  group.id = "the-group";             // test that we can set an id and retrieve the group by its id
+  $(r.canvas).append($(group));
+  var $group = $("#the-group");
+  
+  // reparent every node with the just-created group node.
+  // What's going on here? Raphael puts every VML shape node into a group node which Raphael creates.
+  // However point.node doesn't point at this group node. Instead it points at the shape node inside the group node.
+  // Therefore we find the group node created by Raphael, if it exists, and reparent that with the grouping node *we*
+  // created.
+  
+  $.each(points, function (index, point) {
+    var node, groupNode;
+    
+    if (r.raphael.vml && (groupNode = $(point.node).closest('group')[0])) {
+      node = groupNode;
+    }
+    else {
+      node = point.node;
+    }
+    
+    $group.append($(node)); 
+  });
+  
+  // add style now because it gets lost on reparenting (in IE7)
+  
+  $.each(points, function (index, point) { point.attr(notClicked).attr(notHighlighted); });
+    
   
   // and attach event handlers to the group node
   
@@ -57,6 +82,7 @@ $(function () {
   $group.mousedown(function (evt) {
     $.each(points, addHighlight);       // highlight all the points
     evt.target.raphael.attr(clicked);   // but call out the clicked node (this is event delegation)
+    evt.preventDefault();
     return false;
   });
   
